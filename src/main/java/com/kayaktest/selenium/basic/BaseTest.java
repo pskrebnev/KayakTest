f34954f95609334ea4f571978ca38418fdfbfea2
pskrebnev <Kumbaya33>
2021-12-13 00:12:31 +0100

    Taking screenshot with Shutterbug is implemented

--- src/main/java/com/kayaktest/selenium/basic/BaseTest.java
+++ src/main/java/com/kayaktest/selenium/basic/BaseTest.java
@@ -1,4 +1,35 @@
 package com.kayaktest.selenium.basic;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+
 public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void before() {
+        WebDriverManager.chromedriver ().setup ();
+        ChromeOptions options = new ChromeOptions ();
+        options.setHeadless (false);
+        driver = new ChromeDriver(options);
+        driver.manage ().window ().maximize ();
+
+//        WebDriverManager.edgedriver ().setup ();
+//        EdgeOptions options = new EdgeOptions ();
+//        options.setHeadless (false);
+//        driver = new EdgeDriver (options);
+    }
+
+    @AfterClass
+    public void tierDown() {
+        driver.quit ();
+    }
 }
--- src/test/java/com/kayaktest/selenium/TestProject.java
+++ src/test/java/com/kayaktest/selenium/TestProject.java
@@ -1,11 +1,23 @@
 package com.kayaktest.selenium;
 
+import com.assertthat.selenium_shutterbug.core.Capture;
+import com.assertthat.selenium_shutterbug.core.Shutterbug;
+import com.kayaktest.selenium.basic.BaseTest;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TestProject {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class TestProject extends BaseTest {
+
+    private static final Logger logger = Logger.getLogger(TestProject.class.getName());
 
     @Test
     public void testProject01() {
-        System.out.println ("Print test message");
+        driver.get ("https://google.com");
+        logger.log (Level.INFO, "test me!!!");
+        Assert.assertTrue (true);
+        Shutterbug.shootPage(driver, Capture.FULL_SCROLL).save();
     }
 }
