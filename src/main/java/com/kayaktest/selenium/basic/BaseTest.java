e14955d30648252cd61fb98b0e1ad30d069b8386
pskrebnev <Kumbaya33>
2021-12-12 18:28:48 +0100

    Created Allure dependency

---
+++ src/main/java/com/kayaktest/selenium/basic/BaseTest.java
@@ -0,0 +1,4 @@
+package com.kayaktest.selenium.basic;
+
+public class BaseTest {
+}
---
+++ src/test/java/com/kayaktest/selenium/TestProject.java
@@ -0,0 +1,11 @@
+package com.kayaktest.selenium;
+
+import org.testng.annotations.Test;
+
+public class TestProject {
+
+    @Test
+    public void testProject01() {
+        System.out.println ("Print test message");
+    }
+}
